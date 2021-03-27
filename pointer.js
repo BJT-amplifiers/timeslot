53c557bbce35489800dd67d28d331199555eb2ee
bels44 <79223198+bels44@users.noreply.github.com>
2021-03-27 04:51:03 -0400

    Create pointer.js

---
+++ pointer.js
@@ -0,0 +1,15 @@
+ <script type="text/javascript"> 
+   var table = document.getElementById("tableID");
+    if (table != null) {
+        for (var i = 1; i < table.rows.length; i++) {
+            for (var j = 1; j < table.rows[i].cells.length; j++)
+            table.rows[i].cells[j].onclick = function () {
+                tableText(this);
+            };
+        }
+    }
+
+    function tableText(tableCell) {
+        alert(tableCell.innerHTML);
+    }
+</script>
